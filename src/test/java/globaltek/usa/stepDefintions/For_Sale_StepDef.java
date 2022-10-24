3c8f58db830d7bc38aad2b76d0c1d48d3a620363
qa-alam <alam.itprofessional@gmail.com>
2022-10-24 17:42:57 -0400

    update

--- src/test/java/globaltek/usa/stepDefintions/For_Sale_StepDef.java
+++ src/test/java/globaltek/usa/stepDefintions/For_Sale_StepDef.java
@@ -50,7 +50,7 @@ public class For_Sale_StepDef {
 	public void verify_the_stored_price_with_expected_price() {
 
 	Assert.assertEquals(For_Sale_Page.value, Hooks.driver.findElement(By.xpath("(//*[@class='c-dPyNJo'])[1]")).getText());
-		System.out.println("My Actual result is a : " +For_Sale_Page.value +" My Expected result is a : "+ Hooks.driver.findElement(By.xpath("(//*[@class='c-dPyNJo'])[2]")).getText());
+		System.out.println("My Actual result is a : " +For_Sale_Page.value +" My Expected result is a : "+ Hooks.driver.findElement(By.xpath("(//*[@class='c-dPyNJo'])[1]")).getText());
 	}
 
 	@Then("^Verify the agent informations on the grid$")
