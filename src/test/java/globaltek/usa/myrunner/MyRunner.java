f8a6915fbf1edd7116867bad154984afe7a24320
qa-alam <alam.itprofessional@gmail.com>
2022-10-23 22:47:15 -0400

    Update

--- src/test/java/globaltek/usa/myrunner/MyRunner.java
+++ src/test/java/globaltek/usa/myrunner/MyRunner.java
@@ -4,13 +4,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(	
-plugin= {"pretty","json:target/cucumber.json" },	
-features = ".//Features/", // features = location of Features folder,where i added all feature files
+		plugin = {"pretty","json:target/cucumber.json"},
+		features = ".//Features/", // features = location of Features folder,where i added all feature files
 glue = {"globaltek.usa.stepDefintions","globaltek.usa.hooks"},  
        //glue = step definition location and hooks package location 
-dryRun = false, 
-monochrome = true, 
-strict = false,
 tags = "@Sanity"
 		)
 public class MyRunner extends AbstractTestNGCucumberTests{
